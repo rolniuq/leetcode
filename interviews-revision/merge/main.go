f3cd7890d8643e11dd7893957a2ba4bdf2402c71
rolnuiqdev <truongquynh2525@gmail.com>
2024-07-26 11:50:27 +0700

    chore(interview-revision): day 1

---
+++ interviews-revision/merge/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "sort"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for i := 0; i < n; i++ {
+		nums1[m+i] = nums2[i]
+	}
+
+	sort.Ints(nums1)
+}
+
+func main() {
+	merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+}
