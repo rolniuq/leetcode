0d19c6d9b221ee023e5a8bb55f9f2a5bf225cc40
rolnuiqdev <truongquynh2525@gmail.com>
2024-08-08 17:33:38 +0700

    chore: day 17

--- daily-lessons/longest-one/main.go
+++ daily-lessons/longest-one/main.go
@@ -3,29 +3,34 @@ package main
 import "fmt"
 
 func longestOne(nums []int, k int) int {
-	start := 0
-	end := 0
-	max := 0
+	maxLen := 0
+	curLen := 0
+	exist := k
 
-	temp := k
+	start := 0
+
 	for i := 0; i < len(nums); i++ {
-		if temp == 0 {
-			fmt.Println(start, end)
-			curmax := len(nums[start:end])
-			if curmax > max {
-				max = curmax
-			}
-			temp = k
+		if nums[i] == 0 {
+			exist--
 		}
-		if nums[i] == 1 && start == 0 {
-			start = i
-		} else if temp > 0 {
-			end = i
-			temp--
+
+		if exist >= 0 {
+			curLen++
+		} else {
+			for nums[start] != 0 {
+				start++
+				curLen--
+			}
+			start++
+			exist++
+		}
+
+		if curLen > maxLen {
+			maxLen = curLen
 		}
 	}
 
-	return len(nums[start:end])
+	return maxLen
 }
 
 func main() {
