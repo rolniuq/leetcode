fb015171d9f7a7fe3da5e8b2905aa8258ad9f923
rolnuiqdev <truongquynh2525@gmail.com>
2024-12-22 12:35:01 +0700

    chore: counter 2

--- 30dayjs/counter_2.js
+++ 30dayjs/counter_2.js
@@ -1,18 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
 export function createCounter2(n) {
   var value = n;
 
   return {
     reset() {
-      value = n
-      return value
+      value = n;
+      return value;
     },
     increment() {
       value++;
-      return value
+      return value;
     },
     decrement() {
       value--;
-      return value
+      return value;
     },
   };
 }
