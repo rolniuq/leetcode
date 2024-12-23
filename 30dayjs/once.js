b229558a2a6ce55bc35254484060b44f475dc449
rolnuiqdev <truongquynh2525@gmail.com>
2024-12-24 00:08:40 +0700

    chore: once

--- 30dayjs/once.js
+++ 30dayjs/once.js
@@ -6,18 +6,12 @@
 export const once = (fn) => {
   let wasCalled = false;
 
-  return function(...args){
+  return function (...args) {
     if (wasCalled) {
       return undefined;
     }
 
     wasCalled = true;
     return fn(...args);
-  }
+  };
 };
-
-const fn = (a,b,c) => (a + b + c)
-const onceFn = once(fn)
-
-console.log(onceFn(1,2,3));
-console.log(onceFn(2,3,6));
