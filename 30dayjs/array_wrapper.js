5a98883654d7812a5c53d57c73ff5daf857dcaad
rolnuiqdev <truongquynh2525@gmail.com>
2024-12-28 11:14:29 +0700

    chore: array wrapper

--- 30dayjs/array_wrapper.js
+++ 30dayjs/array_wrapper.js
@@ -3,25 +3,24 @@
  * @return {void}
  */
 export const ArrayWrapper = function (nums) {
-  return function () {};
+  this.nums = nums;
 };
 
 /**
  * @return {number}
  */
 ArrayWrapper.prototype.valueOf = function () {
-  return this.reduce((previous, current) => previous + current, 0);
+  return this.nums.flat().reduce((prev, curr) => prev + curr, 0);
 };
 
 /**
  * @return {string}
  */
-ArrayWrapper.prototype.toString = function () {};
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.nums.toString()}]`;
+};
 
-/**
- * const obj1 = new ArrayWrapper([1,2]);
- * const obj2 = new ArrayWrapper([3,4]);
- * obj1 + obj2; // 10
- * String(obj1); // "[1,2]"
- * String(obj2); // "[3,4]"
- */
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+console.log(String(obj1));
+console.log(obj1 + obj2); // 10
