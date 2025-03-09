304ebd715da34f37507ae531c8f4d25853f7101f
rolnuiqdev <truongquynh2525@gmail.com>
2025-03-09 21:53:44 +0700

    chore: 9 Mar

--- daily-lessons/merge_2_2d_arrays/init.py
+++ daily-lessons/merge_2_2d_arrays/init.py
@@ -19,7 +19,7 @@ class Solution:
     def sort_array(self, nums: List[List[int]]) -> List[List[int]]:
         return sorted(nums, key=lambda x: x[0])
 
-    def merge_array(
+    def mergeArrays(
         self, nums1: List[List[int]], nums2: List[List[int]]
     ) -> List[List[int]]:
         map_num_1 = self.array_to_map(nums1)
@@ -32,3 +32,36 @@ class Solution:
                 map_num_1[key] = map_num_2[key]
 
         return self.sort_array(self.map_to_array(map_num_1))
+
+    def two_pointer(
+        self, nums1: List[List[int]], nums2: List[List[int]]
+    ) -> List[List[int]]:
+        i = 0
+        j = 0
+        result = []
+
+        while (i < len(nums1)) and (j < len(nums2)):
+            id1, val1 = nums1[i]
+            id2, val2 = nums2[j]
+            if id1 < id2:
+                result.append([id1, val1])
+                i += 1
+            elif id1 > id2:
+                result.append([id2, val2])
+                j += 1
+            else:
+                result.append([id1, val1 + val2])
+                i += 1
+                j += 1
+
+        while i < len(nums1):
+            id1, val1 = nums1[i]
+            result.append([id1, val1])
+            i += 1
+
+        while j < len(nums2):
+            id2, val2 = nums2[j]
+            result.append([id2, val2])
+            j += 1
+
+        return result
