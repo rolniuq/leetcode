7d5fc709168fcdf21c500335f9b2893934e75a3e
rolnuiqdev <truongquynh2525@gmail.com>
2025-03-10 21:22:50 +0700

    chore: 10 Mar

--- daily-lessons/lowest_common_ancestor/init.py
+++ daily-lessons/lowest_common_ancestor/init.py
@@ -9,16 +9,15 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        if root == None or p == root or q == root:
+        if not root or root == p or root == q:
             return root
 
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
 
-        if p != None and q != None:
-           return root
-
-        if p != None:
-            return p
-
-        return q
+        if left and right:
+            return root
+        elif left:
+            return left
+        else:
+            return right
