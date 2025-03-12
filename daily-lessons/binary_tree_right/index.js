b4ee85c5ded34cda33a8bac38e2ae644bf56a1a8
rolnuiqdev <truongquynh2525@gmail.com>
2025-03-12 22:38:56 +0700

    chore: 12 Mar

---
+++ daily-lessons/binary_tree_right/index.js
@@ -0,0 +1,23 @@
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const rightSideView = function (root) {
+  if (!root) return [];
+  const res = [];
+  const queue = [root];
+  while (queue.length) {
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      const node = queue.shift();
+      if (i === len - 1) {
+        res.push(node.val);
+      }
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return res;
+};
--- daily-lessons/binary_tree_right/main.go
+++ daily-lessons/binary_tree_right/main.go
@@ -6,69 +6,21 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func getLeftRight(root *TreeNode, arr [][]int) {
+func getNodes(root *TreeNode, level int, arr *[]int) {
 	if root == nil {
 		return
 	}
 
-	res := []int{}
-	if root.Left != nil {
-		res = append(res, root.Left.Val)
-	}
-	if root.Right != nil {
-		res = append(res, root.Right.Val)
-	}
-	if res == nil {
-		return
+	if len(*arr) == level {
+		*arr = append(*arr, root.Val)
 	}
 
-	arr = append(arr, res)
-}
-
-func getLast(arr []int) int {
-	return arr[len(arr)-1]
-}
-
-func getNodes(root *TreeNode, arr [][]int) {
-	if root == nil {
-		return
-	}
-
-	getLeftRight(root, arr)
-	getNodes(root.Left, arr)
-	getNodes(root.Right, arr)
+	getNodes(root.Right, level+1, arr)
+	getNodes(root.Left, level+1, arr)
 }
 
 func rightSideView(root *TreeNode) []int {
-	if root == nil {
-		return nil
-	}
-
-	arr := [][]int{{root.Val}}
-
-	left := root.Left
-	right := root.Right
-	for left != nil || right != nil {
-		if left != nil {
-			arr = append(arr, getLeftRight(left))
-			left = left.Left
-		}
-		if right != nil {
-			arr = append(arr, getLeftRight(right))
-			right = right.Right
-		}
-	}
-
-	res := []int{}
-	for _, v := range arr {
-		if len(v) > 0 {
-			res = append(res, getLast(v))
-		}
-	}
-
-	return res
-}
-
-func main() {
-
+	var arr []int
+	getNodes(root, 0, &arr)
+	return arr
 }
