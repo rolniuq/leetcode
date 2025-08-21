e34f8e21b2a7038489b4e3581abc8de635b9d941
rolniuq <truongquynh2525@gmail.com>
2025-08-21 13:38:49 +0700

    chore: 21 Aug

--- 9.palindrome-number.rs
+++ 9.palindrome-number.rs
@@ -8,20 +8,19 @@
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
         if x < 0 {
-            return false
+            return false;
         }
 
         let mut y = x;
         let mut res = 0;
 
         while y > 0 {
-            res = res * 10 + y%10;
+            res = res * 10 + y % 10;
             y /= 10;
         }
 
-        return res == x
+        return res == x;
     }
 }
 
 // @lc code=end
-
