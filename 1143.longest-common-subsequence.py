1ab438f330bf86fedbf1d132803d469c795b09a7
rolniuq <truongquynh2525@gmail.com>
2025-12-03 17:12:01 +0700

    chore: 3 Dec

--- 1143.longest-common-subsequence.py
+++ 1143.longest-common-subsequence.py
@@ -65,19 +65,40 @@
 
 # @lc code=start
 class Solution:
+    def dp(
+        self, i: int, j: int, text1: str, text2: str, memo: dict[tuple[int, int], int]
+    ) -> int:
+        if i == len(text1) + 1 or j == len(text2) + 1:
+            return 0
+
+        if (i, j) in memo:
+            return memo[(i, j)]
+        if text1[i - 1] == text2[j - 1]:
+            memo[(i, j)] = self.dp(i + 1, j + 1, text1, text2, memo) + 1
+        else:
+            memo[(i, j)] = max(
+                self.dp(i + 1, j, text1, text2, memo),
+                self.dp(i, j + 1, text1, text2, memo),
+            )
+
+        return memo[i, j]
+
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n = len(text1)
-        m = len(text2)
-        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        memo = {}
+        return self.dp(1, 1, text1, text2, memo)
 
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-                else:
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        # n = len(text1)
+        # m = len(text2)
+        # dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-        return dp[n][m]
+        # for i in range(1, n + 1):
+        #     for j in range(1, m + 1):
+        #         if text1[i - 1] == text2[j - 1]:
+        #             dp[i][j] = dp[i - 1][j - 1] + 1
+        #         else:
+        #             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        # return dp[n][m]
 
 
 # @lc code=end
